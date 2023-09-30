d689a41248792f5cb0a4b08f878b5d6dc6912907
xiaochuan1208 <xiaochuan.wang@studytafensw.edu.au>
2023-10-01 00:35:54 +1000

    sports warehouse

---
+++ sports-warehouse/menu.js
@@ -0,0 +1,38 @@
+// Get the menu toggle and menu ((from the DOM)
+const menuToggle = document.querySelector(".top-nav-toggle");
+const menu = document.querySelector(".top-nav-menu");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  // Check that the elements exist (menu and menu toggle)
+  if (menuToggle && menu) {
+    
+    // if the menu is open,  close it
+    if (menu.classList.contains("active")) {
+      //remove the "active" class from the menu
+      menu.classList.remove("active");
+
+      // put font-awesome menu icon in the toggle element
+      menuToggle.querySelector("a").innerHTML = ' <div class="pre-icon"><span class="sr-only">Open Menu</span><i class="fas fa-bars fa-lg" aria-label="Open Menu"></i></div><p>Menu</p>';
+
+      // set the aria expanded attribute to false
+      menuToggle.setAttribute("aria-expanded", "false");
+    }
+
+    // otherwise open it
+    else {
+      //add the "active" class to the menu
+      menu.classList.add("active");
+
+      // put font-awesome close icon in the toggle element
+      menuToggle.querySelector("a").innerHTML = '<span class="sr-only">Menu Close</span><i class="fas fa-times fa-lg" aria-label="Close Menu"></i>';
+      
+
+      // set the aria expanded attribute to true
+      menuToggle.setAttribute("aria-expanded", "true");
+    }
+  }
+}
+
+/* Event Listeners */
+menuToggle.addEventListener("click", toggleMenu, false);
